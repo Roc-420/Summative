4d088ddffb9aad22d347609d4445420e08f2937a
Roc-420 <nlast031@gmail.com>
2025-01-17 10:02:58 -0500

    add ability to add customers and stocks

--- app.py
+++ app.py
@@ -27,8 +27,9 @@ def get_lines(l): # converts maze txt into array
     return lister
 
 
-
-
+def write_line(file,line):
+    with open(file, "a") as myfile:
+        myfile.write(line+ "\n")
 @app.route("/", methods = ['GET','POST'])
 def initial():
     session['inputs'] = []
@@ -70,8 +71,41 @@ def cust():
 
 
 
+@app.route("/add_stocks", methods = ['GET','POST'])
+def stox():
+    session['inputs'] = []
+    # this is the home login page 
+    if request.method == "POST":
+        for index,value in request.form.items():
+            session['inputs'].append(value)
+            print(session['inputs'])
+        
+        if 4 < len(session['inputs'][0]) < 20 and session['inputs'][1].isalnum and session['inputs'][2].isalnum:
+            print("passed first test!")
+            if 0 < int(session['inputs'][1]) < 250:
+                print("passed second test!")
+                if  0 < int(session['inputs'][2]) < 250:
+                    print("passed final test!")
+                    linee = session['inputs'][0] + "," + session['inputs'][1] + "," + session['inputs'][2] 
+                    print(linee)
+                    write_line('stocks.txt', linee  )
+ 
 
+    return render_template('add_stocks.html')
+        
 
+@app.route('/add_customers' , methods = ['GET','POST'])
+def add_customer():
+    session['inputs'] = []
+    if request.method == "POST":
+        for index,value in request.form.items():
+            session['inputs'].append(value)
+            print(session['inputs'])
+        linee = session['inputs'][0] + "," + session['inputs'][1] 
+        print(linee)
+        write_line('customers.txt', linee  )
+    
+    return render_template('add_customers.html')
 
 
 
