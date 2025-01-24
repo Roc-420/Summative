5681367f9c0bbd955fb3baf481c01e33051a4e5e
Roc-420 <nlast031@gmail.com>
2025-01-24 00:08:55 -0500

    finished
    adding selling stocks and transaction recording

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 
 
 from flask import redirect,render_template,url_for, Flask,request, session
-
+import datetime
 
 app = Flask(__name__)
 app.secret_key = "hello"
@@ -12,29 +12,16 @@ admin_user = "sigma"
 admin_password = "1234"
 
 
-def get_lines(l): # converts maze txt into array
-    lister = []
-    line=""
-    txt = open(l,'r')
-    content = txt.readlines()
-    txt.close()
-    for row in content:
-        for item in row:
-            line = line + item
-        lister.append(line)
-        line = ""
-    
-    return lister
-
+def get_lines(filename):
+    with open(filename, 'r') as f:
+        return [line.strip() for line in f]  # Strips leading/trailing whitespace
 
 def write_line(file,line):
     with open(file, "a") as myfile:
-        myfile.write(line+ "\n")
+        myfile.write(line + '\n')
 
 
 
-
-        
 @app.route("/", methods = ['GET','POST'])
 def initial():
     session['inputs'] = []
@@ -74,6 +61,42 @@ def cust():
         
 
 
+@app.route("/veiw stocks", methods = ['GET','POST'])
+def stoxxer():
+    current_date= datetime.datetime.now()
+    session['inputs'] = []
+    stox_list = get_lines('stocks.txt')
+    # this is the home login page 
+    if request.method == "POST":
+        for index,value in request.form.items():
+            session['inputs'].append(value)
+            print(session['inputs'])
+            print(len(stox_list))
+            print(stox_list)
+        if 1 <= int(session['inputs'][0]) <= len(stox_list):
+            print("satisfied")
+            replacing_item = int(session['inputs'][0]) - 1
+            print(stox_list[replacing_item])
+            name,price, amount = stox_list[replacing_item].split(',')
+            print(name,price,amount)
+            amount = int(amount) - 1
+            if amount > 0:
+                stox_list[replacing_item] = f"{name},{price},{amount}"
+                print('replacing')
+            else:
+                stox_list.pop(replacing_item)
+            print("stock_list_1",stox_list)
+            with open('stocks.txt', "w") as myfile:
+                for line in stox_list:
+                    if len(line) > 2:
+                        myfile.write(line + '\n')
+            stox_list = get_lines('stocks.txt')
+            print("stock_list_2",stox_list)
+            
+            write_line('transactions.txt', name + " at " + current_date.strftime("%d-%m-%Y") )
+
+    return render_template('veiw_stocks.html',transer = stox_list)
+        
 
 
 @app.route("/add_stocks", methods = ['GET','POST'])
@@ -95,7 +118,8 @@ def stox():
                     print(linee)
                     write_line('stocks.txt', linee  )
  
-
+    stox_list = get_lines('stocks.txt')
+    print(stox_list)
     return render_template('add_stocks.html')
         
 
